1587b21549940db8cd6529ae0be4bd122a92f415
Kevin Pes <kevinecko94@gmail.com>
2022-10-19 10:24:56 +0200

    Update execute.bat

--- execute.bat
+++ execute.bat
@@ -2,8 +2,7 @@ cd Rider
 call executeRider.bat
 cd ../Restaurant
 call executeRes.bat
-cd ../BankService
-call executeBank.bat
 cd ../GIS
 call executeGIS.bat
-
+cd ../BankService
+call executeBank.bat
